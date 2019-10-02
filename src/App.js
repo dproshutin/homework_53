f3ea8563bcedc1b2e109413a42795ed76308c084
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-03 00:52:41 +0600

    The state is initialized with three tasks in the main component App.js. Tasks are set as array of objects with 'text' and 'id' keys. Date.now() function is used to set id value of existing tasks. Also, currentTask key with null value is added to the state.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import AddTaskForm from './AddTaskForm/AddTaskForm.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: [
+      {text: "Buy milk", id: 1570041853066},
+      {text: "Walk with dog", id: 1570041880519},
+      {text: "Do homework", id: 1570041920486}
+    ],
+    currentTask: null
+  };
+  render() {
+    return (
+    );
+  }
 }
 
 export default App;
